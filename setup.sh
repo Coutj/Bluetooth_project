899b78024af06ec137a658805932c6727591ab79
Marcos Méndez Quintero <38361760+marcos-mendez@users.noreply.github.com>
2022-12-06 17:48:20 -0300

    alias to pip
    Common guys already 2023 almost. PIP is PIP3

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo apt install libbluetooth-dev
-sudo pip3 install -r requirements.txt
+sudo apt install libbluetooth-dev pip
+alias pip=pip3
+sudo pip install -r requirements.txt
 #etc.
